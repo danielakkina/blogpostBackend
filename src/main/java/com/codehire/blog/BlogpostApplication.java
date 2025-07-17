f3b80659f6fe8a72d89aa709073e720e56b6fabd
DanielAkkina <danielakkina@gmail.com>
2025-07-17 16:41:33 +0530

    print env variables in main for debugging

--- src/main/java/com/codehire/blog/BlogpostApplication.java
+++ src/main/java/com/codehire/blog/BlogpostApplication.java
@@ -3,8 +3,19 @@ package com.codehire.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
+
+
+
 @SpringBootApplication
 public class BlogpostApplication {
+	@PostConstruct
+	public void printEnv() {
+		System.out.println("🟡 JDBC_DATABASE_URL = " + System.getenv("JDBC_DATABASE_URL"));
+		System.out.println("🟡 JDBC_DATABASE_USERNAME = " + System.getenv("JDBC_DATABASE_USERNAME"));
+		System.out.println("🟡 JDBC_DATABASE_PASSWORD = " + System.getenv("JDBC_DATABASE_PASSWORD"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogpostApplication.class, args);
